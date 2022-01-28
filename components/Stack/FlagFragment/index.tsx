58927ecb0fa670f0cf9c17863117ffe1433a64c3
omarmhmmd <omar.mhmmd@gmail.com>
2022-01-27 22:31:49 -0800

    merging to main

--- components/Stack/FlagFragment/index.tsx
+++ components/Stack/FlagFragment/index.tsx
@@ -1,40 +1,43 @@
 import styles from "./flagFragment.module.scss";
 import Image from "next/image";
+import Link from "next/link";
 import SmallTextBox from "../../SmallTextBox";
 import { NextPage } from "next";
 import React, { useState } from "react";
 
 interface Props {
   image: string;
-	duration: string;
+  duration: string;
   kingdom: string;
 }
 
 const FlagFragment: NextPage<Props> = (props) => {
-
   return (
     <>
-      <div className={styles.container} >
-        <Image
-          src={props.image}
-          className={styles.image}
-          layout="fill"
-          objectFit="cover"
-          alt="Fragment of a flag in a list of other fragments"
-        />
-        <div className={styles.infoContainer}>
-					<div>
-						<SmallTextBox duration={props.duration} />
-						<SmallTextBox kingdom={props.kingdom} />
-					</div>
-					<div>
-						<SmallTextBox type={"drag"} />
-					</div>
+      <Link href="/flag/year">
+        <div className={styles.container}>
+          <Image
+            src={props.image}
+            className={styles.image}
+            layout="fill"
+            objectFit="cover"
+            alt="Fragment of a flag in a list of other fragments"
+          />
+          <div className={styles.infoContainer}>
+            <div>
+              <SmallTextBox duration={props.duration} />
+              <SmallTextBox kingdom={props.kingdom} />
+            </div>
+            <div>
+              <Link href="/flag/year">
+                <SmallTextBox type={"drag"} />
+              </Link>
+            </div>
+          </div>
         </div>
-      </div>
+      </Link>
     </>
   );
 };
 
 export default FlagFragment;
-
