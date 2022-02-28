f8b3ff8363acbf09de7e556ad045b68591f267ba
Omar Mohammad <omar.mhmmd@gmail.com>
2022-02-27 23:41:21 -0800

    [FOA-4] Update img textures on click (#4)
    * changing texture live on click
    
    * parsing int
    
    * got routing working again
    
    * got image update; merging to FOA-4

--- pages/flag/[flagIndex]/year/[year].jsx
+++ pages/flag/[flagIndex]/year/[year].jsx
@@ -1,12 +1,33 @@
+import { useRouter } from "next/router";
 import React from "react";
+import styles from "./flag.module.scss";
+import Title from "../../../../components/Title";
+import MUIGrid from "@mui/material/Grid";
+import Flag from "../../../../components/Flag";
+import { flags } from "../../../../public/flags.js";
+import Data from "../../../../components/Data";
+import ThreeJS from "../../../../components/ThreeJS";
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
-import { MathUtils } from "three";
-import styles from "./threeJS.module.scss";
 
-const ThreeJS = (props) => {
+export async function getServerSideProps() {
+  return {
+    props: {},
+  };
+}
+
+const Index = () => {
+  const router = useRouter();
+  const { flagIndex, year } = router.query;
+
+  const flagIndexNum = parseInt(flagIndex);
+
+  const checkFlag = () => {
+    return flagIndexNum - 1;
+  };
+
   const flagRef = useRef(null);
-	const controls = useRef(null);
+  const controls = useRef(null);
 
   useEffect(() => {
     var DAMPING = 0.03;
@@ -273,7 +294,7 @@ const ThreeJS = (props) => {
       // cloth material
       THREE.ImageUtils.crossOrigin = "";
       var clothTexture = THREE.ImageUtils.loadTexture(
-        "/images/flags-threeJS/" + props.meshIndex + ".jpg"
+        "/images/flags-threeJS/" + checkFlag() + ".jpg"
       );
       clothTexture.wrapS = THREE.RepeatWrapping;
       clothTexture.anisotropy = 16;
@@ -291,7 +312,7 @@ const ThreeJS = (props) => {
         side: THREE.DoubleSide,
         needsUpdate: true,
       });
-			clothTexture.needsUpdate=true
+      clothTexture.needsUpdate = true;
 
       // cloth geometry
       clothGeometry = new THREE.ParametricGeometry(
@@ -317,18 +338,18 @@ const ThreeJS = (props) => {
 
       // cloth mesh
       object = new THREE.Mesh(clothGeometry, clothMaterial);
-			object.material.map.needsUpdate=true;
+      object.material.map.needsUpdate = true;
       object.position.set(-118, 50, 0);
 
-			object.traverse(function(child) {
-				if(child instanceof THREE.Mesh){
-						// alert("works");
-						child.material.map = clothTexture;
-						child.material.needsUpdate = true;
-						child.geometry.buffersNeedUpdate = true;
-						child.geometry.uvsNeedUpdate = true;
-				}
-		});
+      object.traverse(function (child) {
+        if (child instanceof THREE.Mesh) {
+          // alert("works");
+          child.material.map = clothTexture;
+          child.material.needsUpdate = true;
+          child.geometry.buffersNeedUpdate = true;
+          child.geometry.uvsNeedUpdate = true;
+        }
+      });
 
       /**
        * Mobile
@@ -381,7 +402,7 @@ const ThreeJS = (props) => {
         flagRef.current.clientHeight
       );
       renderer.setClearColor(scene.fog.color);
-			// renderer.setPixelRatio(0.2 );
+      // renderer.setPixelRatio(0.2 );
 
       flagRef.current.appendChild(renderer.domElement);
 
@@ -455,8 +476,8 @@ const ThreeJS = (props) => {
       oneStep(time);
     }
 
-		var changeFlag = (flagNum) => {
-			object.material.map=THREE.ImageUtils.loadTexture(
+    var changeFlag = (flagNum) => {
+      object.material.map = THREE.ImageUtils.loadTexture(
         "/images/flags-threeJS/" + flagNum + ".jpg"
       );
     };
@@ -465,22 +486,98 @@ const ThreeJS = (props) => {
 
     init();
     go();
-  }, [props.meshIndex]);
+  }, []);
   return (
-    <>
-      <div ref={flagRef} className={styles.container}>
-			<button
-        onClick={() => {
-          controls.current.changeFlag(4);
-        }}
+    <MUIGrid className={styles.container} container spacing={0}>
+      <MUIGrid
+        item
+        className={styles.listContainer}
+        sm={1.5}
+        display={{ xs: "none", sm: "block" }}
       >
-        changeFlag
-      </button>
-        {props.meshIndex}
-      </div>
-      ;
-    </>
+        <div className={styles.title} style={{ backgroundColor: "white" }}>
+          <Title farsi="بيرق هاى افغانستان" english="Flags Of Afghanistan" />
+        </div>
+        <div className={styles.list}>
+          {flags.map((flag, index) => (
+            <div
+              onClick={() => {
+                controls.current.changeFlag(index);
+              }}
+              key={index}
+              className={styles.listItem}
+            >
+              <Flag
+                key={index}
+                image={`/images/flags-sml/${index}.jpg`}
+                duration={flag.duration}
+                kingdom={flag.kingdom}
+                year={flag.year}
+                flagIndex={index}
+              />
+            </div>
+          ))}
+        </div>
+      </MUIGrid>
+      <MUIGrid className={styles.threeJSContainer}>
+        <div ref={flagRef} className={styles.container}></div>;
+        {/* <ThreeJS meshIndex={checkFlag()} /> */}
+      </MUIGrid>
+      <MUIGrid item xs={12} sm={3} className={styles.infoContainer}>
+        <div className={styles.title}>
+          <Title
+            farsi="جمهوری دمکراتی افغانستان"
+            english={flags[checkFlag()].kingdom}
+          />
+        </div>
+        <div className={styles.infoList}>
+          <div className={styles.infoFlag}>
+            <img src={`/images/flags-sml/${checkFlag()}.jpg`} alt="Flag" />
+          </div>
+          <div className={styles.infoData}>
+            <Data
+              duration={flags[checkFlag()].duration}
+              kingdom={flags[checkFlag()].kingdom}
+              ruler={flags[checkFlag()].ruler}
+              year={flags[checkFlag()].year}
+            />
+          </div>
+          <div className={styles.infoEssay}>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum. voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit
+              anim id est laborum.
+              <br />
+              <br />
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum. voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit
+              anim id est laborum.
+            </p>
+          </div>
+        </div>
+      </MUIGrid>
+    </MUIGrid>
   );
 };
 
-export default ThreeJS;
+export default Index;
